024d4173e62da3662a34cf20c893c15fe52990d6
kevin-kele <kevinkele1@gmail.com>
2021-02-10 17:33:12 +0100

    params get

--- app.js
+++ app.js
@@ -3,15 +3,37 @@ require('babel-register')
 const express = require('express') 
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log("URL :"+req.url)
-    next()
-})
+const morgan = require('morgan')
+
+
+const members= [
+    {
+        id:1,
+        name:"kevin"
+    },
+    {
+        id:2,
+        name:"junior"
+    },
+    {
+        id:3,
+        name:"keyate"
+    },
+]
+
+
+app.use(morgan("dev"))
+
 app.get('/api',(req,res)=>{
     res.send('Root API')
 })
+
 app.get('/api/book/:id',(req,res)=>{
     res.send(req.params)
+})
+
+app.get('/api/members/:id',(req,res)=>{
+    res.send(members[(req.params.id)-1])
 })
 
 
