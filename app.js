20311b72f4c855b6bdeed21d297a2b94778efa51
kevin-kele <kevinkele1@gmail.com>
2021-02-10 17:03:31 +0100

    middlewares

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ require('babel-register')
 const express = require('express') 
 const app = express()
 
+app.use((req,res,next)=>{
+    console.log("URL :"+req.url)
+    next()
+})
 app.get('/api',(req,res)=>{
     res.send('Root API')
 })
 app.get('/api/book/:id',(req,res)=>{
     res.send(req.params)
 })
+
+
 app.listen('8080',()=>{
     console.log('started 8080')
 })
